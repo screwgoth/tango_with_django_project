f4dc7a4ee6ae30a71a1c9c8421255cb2a3cf47a7
screwgoth <raseelbhagat@gmail.com>
2016-09-12 13:11:09 +0530

    Added Django Registration Redux package. Completed Chapter 12

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistratioView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +16,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistratioView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 if settings.DEBUG:
